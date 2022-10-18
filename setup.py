38bee15fb24321fc069774126a4ff06c1317ab2c
Ahmed Abbas <ahmedabb@axis.com>
2022-10-18 15:02:17 +0200

    test adding init

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name="pip-example",
       version="1.0.0",
       license='MIT',
       url='https://github.com/ahmed91abbas/pip-example',
-      install_requires=['requests'],
+      # install_requires=['requests'],
       packages=['domain'],
       package_dir={'domain': 'src/domain'},
 )
